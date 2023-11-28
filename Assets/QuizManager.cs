bcbb6658eb6667aa26f21673ec6d7c44eed065e5
pedrogaldo <101400428+pedrogaldo@users.noreply.github.com>
2023-11-28 02:31:32 -0300

    Initial commit

---
+++ Assets/QuizManager.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuizManager : MonoBehaviour
+{
+    public List<QuestionsAndAnswers> QnA;
+    public GameObject[] options;
+    public int currentQuestion;
+
+    public Text QuestionTxt;
+
+
+    //audios
+
+    public AudioClip pregunta1;
+    public AudioClip pregunta2;
+    public AudioClip pregunta3;
+    public AudioClip pregunta4;
+    public AudioClip pregunta5;
+
+    private void Start()
+    {
+        generateQuestion();
+    }
+
+    public void correct()
+    {
+        QnA.RemoveAt(currentQuestion);
+        generateQuestion();
+    }
+
+    void SetAnswers()
+    {
+        for (int i = 0; i < options.Length; i++)
+        {
+            options[i].GetComponent<AnswerScript>().isCorrect = false;
+            options[i].transform.GetChild(0).GetComponent<Text>().text = QnA[currentQuestion].Answers[i];
+
+            if(QnA[currentQuestion].CorrectAnswer == i + 1) 
+            {
+                options[i].GetComponent<AnswerScript>().isCorrect = true;
+            }
+        }
+
+        if (currentQuestion == 1)
+        {
+
+        }
+    }
+
+    void generateQuestion()
+    {
+        currentQuestion = Random.Range(0, QnA.Count);
+
+        QuestionTxt.text = QnA[currentQuestion].Question;
+        SetAnswers();
+
+        QnA.RemoveAt(currentQuestion);
+    }
+}
